881fdb1e0429d5b69c039ea21e8d7114affaa920
shravan3008 <70143163+shravan3008@users.noreply.github.com>
2023-12-18 14:19:11 +0530

    Update backend.tf
    updated bucket name

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "super-mario-bucket-1717" # Replace with your actual S3 bucket name
+    bucket = "super-mario-bucket-1804" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
